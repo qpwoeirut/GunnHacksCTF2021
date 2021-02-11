7c36c34ff59771c064de339d21d405c503e11311
qpwoeirut <stanleyzhong8@gmail.com>
2021-02-11 12:01:33 -0800

    add rules check chall

---
+++ rules_check/challenge.py
@@ -0,0 +1,9 @@
+from hacksport.problem import Challenge, File
+
+
+class Problem(Challenge):
+    def generate_flag(self, random):
+        return "gunnHacks{g00d_luck_4nd_h4v3_fun!}"
+
+    def setup(self):
+        pass
