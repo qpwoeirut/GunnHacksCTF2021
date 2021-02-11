5ed55e8b8b24512beba08f6e4fdc83a5694cffec
chop0 <alecthechop@gmail.com>
2021-02-11 21:40:09 +0000

    rewrote in assembly

--- lemongallery/main.c
+++ lemongallery/main.c
@@ -1,11 +1,10 @@
-#include <unistd.h>
-
+#include "syscalls.h"
 char* fuckyougcc = "pepega";
 char* lemon = "\xff\xe1";
 
 void vuln() {
 char buf[32];
-read(STDIN_FILENO, buf, 256);
+read(0, buf, 256);
 }
 
 int main() {
