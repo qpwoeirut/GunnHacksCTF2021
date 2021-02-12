39ba75ee660efd50dc61917a46a5d936cf764a70
qpwoeirut <stanleyzhong8@gmail.com>
2021-02-12 12:00:56 -0800

    fix challenge.py

--- taking_a_walk/challenge.py
+++ taking_a_walk/challenge.py
@@ -2,7 +2,7 @@ from hacksport.problem import Challenge, File
 
 
 class Problem(Challenge):
-    files = [File("img1.jpg"), File("img2.jpg"), File("img3.jpg"), File("img4.jpg"), File("img5.jpg"), File("img6.jpg"), File("img7.jpg")]
+    files = [File("img.zip")]
 
     def generate_flag(self, random):
         return "gunnHacks{geotags}"
