5c5d3eae9e02519a8f3a28b8249e5450c22852d3
qpwoeirut <stanleyzhong8@gmail.com>
2021-02-11 23:20:08 -0800

    add intro to netcat chall, change rules check desc

---
+++ intro_to_netcat/challenge.py
@@ -0,0 +1,8 @@
+from hacksport.problem import Remote, File, ProtectedFile
+
+class Problem(Remote):
+    program_name = "intro_to_netcat.py"
+
+    def generate_flag(self, random):
+        hexdigits = hex(random.randrange(16 ** 8))[2:]
+        return "gunnHacks{int3rn3t_c4t$_" + hexdigits + '}'
